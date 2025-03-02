88299e669f6d192f726cebaa2888cfb27ae3d134
romainducrocq <romain.g.ducrocq@gmail.com>
2025-03-02 22:35:13 +0100

    patch nintaco archive

--- bin/make.sh
+++ bin/make.sh
@@ -3,7 +3,7 @@
 OPT=$(dirname $(pwd))'/opt/'
 
 sudo apt-get update
-sudo apt-get install g++ cmake valgrind libgtest-dev wmctrl
+sudo apt-get install g++ cmake libsfml-dev valgrind libgtest-dev wmctrl
 
 cd /usr/src/gtest
 sudo cmake CMakeLists.txt
@@ -17,10 +17,5 @@ tar zxvf jre-8uLatest-linux-x64.tar.gz
 rm -v jre-8uLatest-linux-x64.tar.gz
 mv -v jre1.8* jre1.8.0_latest
 
-cd "${OPT}"'nintaco'
-rm -rv *
-mkdir -p Nintaco_bin_2020-05-01
-cd Nintaco_bin_2020-05-01
-wget https://nintaco.com/Nintaco_bin_2020-05-01.zip
+if [ -d "Nintaco_bin_2020-05-01" ]; then rm -rv Nintaco_bin_2020-05-01; fi
 unzip Nintaco_bin_2020-05-01.zip
-rm -v Nintaco_bin_2020-05-01.zip
